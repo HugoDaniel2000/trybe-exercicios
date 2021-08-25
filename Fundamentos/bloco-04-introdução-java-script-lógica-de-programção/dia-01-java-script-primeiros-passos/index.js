49f1f150aa23b724597ff1d39e6465b14819c83e
HugoDaniel2000 <hugodanielc67@gmail.com>
2021-08-25 20:08:46 -0300

    exercicio_4.1

---
+++ Fundamentos/bloco-04-introdução-java-script-lógica-de-programção/dia-01-java-script-primeiros-passos/index.js
@@ -0,0 +1,138 @@
+//    tarefa 1
+ //const a = 10;
+ //const b = 5;
+
+ //console.log(a + b);
+ //console.log(a - b);
+ //console.log(a * b);
+ //console.log(a / b);
+//
+
+//     tarefa 2 e 3
+//const c = 15
+ //if (a>b && a>c){
+  //console.log(a);}
+ //else if (b>c && b>a) {
+  //console.log(b);}
+ //else{
+    //console.log(c);}
+
+//
+
+
+//     tarefa 4
+//const a = 10;
+
+ //if (a>0) {
+  //console.log("positivo");}
+
+ //else if(a<0){
+  //console.log("negativo");}
+
+ //else{
+  //console.log("zero");}
+//
+
+
+
+//    tarefa 5
+ //const a = 60;
+ //const b = 60;
+ //const c = 60;
+
+ //if (a + b + b=== 180){
+   //console.log(true);}
+
+ //else{
+   //console.log(false);}
+//
+
+
+
+//   tarefa 6
+  //let peças = "CAVALO";
+  
+
+  //if(peças =="bispo"){
+    //console.log("diagonais");}
+
+  //else if (peças == "cavalo"){
+    //console.log("L");}
+
+  //else if (peças == "rei"){
+    //console.log("qualquer direção com limite de uma casa");}
+
+  //else if (peças =="rainha"){
+    //console.log("qualquer direção sem limites de casa");}
+
+  //else{
+  //console.log("erro");}
+//
+
+
+
+
+
+//TAREFA 7
+//const nota = 80;
+ //const porcento = (nota / 100) * 100;
+
+ //if(porcento >= 90){
+   //console.log("A");}
+
+ //else if (porcento >= 80){
+   //console.log("B");}
+
+ //else if (porcento >= 70){
+   //console.log("C");}
+
+ //else if (porcento >= 60){
+   //console.log("D");}
+
+ //else if (porcento >= 50){
+   //console.log("E");}
+
+ //else if(nota <0 || nota > 100){
+   //console.log("erro")}
+
+ //else{
+   //console.log("F");}
+//
+
+
+// tarefa 8
+  //const a = 1;
+  //const b = 5;
+  //const c = 3;
+
+  //if (a%2==0 || b%2==0 || c%2==0){
+  //console.log(true);}
+  //else{
+  //console.log(false);}
+//
+
+
+
+// tarefa 9
+  //const a = 12;
+  //const b = 2;
+  //const c = 4;
+
+  //if (a%2!=0 || b%2!=0 || c%2!=0){
+      //console.log(true);}
+  //else{
+      //console.log(false);}
+//
+  
+
+
+//tarefa 10
+  const cp = 3;
+  const vv = 5 * 1000;
+
+  var imp = cp * (20 / 100);
+  var ct = cp + imp;
+  var lucro = vv - ct;
+
+  console.log(lucro);
+//  
