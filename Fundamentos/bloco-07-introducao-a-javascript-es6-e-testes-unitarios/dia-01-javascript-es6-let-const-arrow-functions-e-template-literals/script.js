c43527b57ba60f3e5b77a9458453bfdc9c76079b
HugoDaniel2000 <hugodanielc67@gmail.com>
2021-09-23 19:25:35 -0300

    exercicio 7.1

--- Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ Fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,44 +1,44 @@
-// const testingScope = (escopo) => {
-//   if (escopo === true) {
-//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-//     console.log(ifScope);
-//   } else {
-//     const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-//     console.log(elseScope);
-//   }
-// }
-// testingScope(true);
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+testingScope(true);
 
-// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// const sortOddsAndEvens = () => {
-//   oddsAndEvens[0] = 2;
-//   oddsAndEvens[1] = 3;
-//   oddsAndEvens[2] = 4;
-//   oddsAndEvens[3] = 7;
-//   oddsAndEvens[4] = 10;
-//   oddsAndEvens[5] = 13;
+const sortOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
 
-//   return oddsAndEvens;
-// }
+  return oddsAndEvens;
+}
 
-// const sortedArray = sortOddsAndEvens();
-// console.log(`Os números ${oddsAndEvens.sort(function(a, b){return a-b})} se encontram ordenados de forma crescente !`);
+const sortedArray = sortOddsAndEvens();
+console.log(`Os números ${oddsAndEvens.sort(function(a, b){return a-b})} se encontram ordenados de forma crescente !`);
 
-// const fatorial = (n) => {
-//   let fator = 1;
-//   for (let index = 2; index <= n; index += 1) {
-//      fator = fator * index;
-//   }
-//   return fator
-// }
+const fatorial = (n) => {
+  let fator = 1;
+  for (let index = 2; index <= n; index += 1) {
+     fator = fator * index;
+  }
+  return fator
+}
 
-// console.log(fatorial(8));
+console.log(fatorial(8));
 
-// const fatorialRecursivo = (n) => n > 1 ? n * fatorialRecursivo(n-1) : 1;
+const fatorialRecursivo = (n) => n > 1 ? n * fatorialRecursivo(n-1) : 1;
 
-// console.log (fatorialRecursivo(5))
+console.log (fatorialRecursivo(5))
 
 const maiorPalavra = (string) => {
   const split = string.split(' ');
@@ -53,4 +53,4 @@ const maiorPalavra = (string) => {
   return bigWord
 }
 
-console.log(maiorPalavra("andre é foda demais dasdasdasdas"));
+console.log(maiorPalavra("andre é foda demais"));
