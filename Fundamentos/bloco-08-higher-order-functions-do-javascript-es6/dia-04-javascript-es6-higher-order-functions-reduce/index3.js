c308131fdb738f93a31fa80d0cfd87e651843acc
HugoDaniel2000 <hugodanielc67@gmail.com>
2021-10-07 20:33:25 -0300

    exercicio 8.4

---
+++ Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/index3.js
@@ -0,0 +1,30 @@
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+ return names.reduce((previous, current) => {
+    return previous + current.toLowerCase().split('a').length - 1
+  }, 0)
+}
+
+console.log(containsA());
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+ return students.map((student, index) => ({
+    name: student,
+    average: grades[index].reduce((previous, current ) => previous + current,0) / grades[index].length,
+  }))
+}
+
+console.log(studentAverage());
